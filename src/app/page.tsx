b96d7045b6f8886e4eb1a0c5ceae6304911e3ec1
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-03-02 20:58:19 +0300

    test ssh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/shared/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Touch me!</Button>
+      <Button>Click me!</Button>
     </main>
   );
 }
