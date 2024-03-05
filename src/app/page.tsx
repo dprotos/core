88f2646bc8552904f5c7fec6cc5ec4cbe1667c5f
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-03-05 17:58:28 +0300

    chor: хрень какая-то с ветками

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8">
+      <h1>Courses</h1>
       <CreateCourseForm
         className="w-[600px] mb-5 mx-auto"
         revalidatePagePath="/"
