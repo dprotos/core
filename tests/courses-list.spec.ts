c47de2c0169b89857332a921ba6217d15214f373
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-03-05 23:08:10 +0300

    ci: fix TEST_ENV_BASE_URL

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
+import "dotenv/config";
 
 test("test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto(process.env.TEST_ENV_BASE_URL!);
   await page.getByPlaceholder("название").click();
   await page.getByPlaceholder("название").fill("First test");
   await page.getByPlaceholder("описание").click();
