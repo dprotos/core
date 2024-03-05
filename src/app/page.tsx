11274143310255095f9978c266376f099f45bd5d
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-03-05 17:51:29 +0300

    chor: change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8">
-      <CreateCourseForm className="max-w-[300px] mb-5" revalidatePagePath="/" />
-      <CoursesList revalidatePagePath="/" />
+      <CreateCourseForm
+        className="w-[600px] mb-5 mx-auto"
+        revalidatePagePath="/"
+      />
+      <CoursesList className="w-[600px] mx-auto" revalidatePagePath="/" />
     </main>
   );
 }
