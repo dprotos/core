53dad1fe2f769a675fc30fb595b90256f173b482
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-03-07 20:31:39 +0300

    feat: mail auth

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { dbClient } from "../../shared/lib/db";
 import { privateConfig } from "@/shared/config/private";
@@ -8,6 +9,17 @@ import { compact } from "lodash-es";
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
   providers: compact([
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST,
+        port: parseInt(privateConfig.EMAIL_SERVER_PORT),
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: privateConfig.EMAIL_FROM,
+    }),
     privateConfig.GITHUB_ID &&
       privateConfig.GITHUB_SECRET &&
       GitHubProvider({
